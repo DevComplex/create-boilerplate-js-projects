9e7613040052b1f508c5b25fd524a09aa4b1df89
Pedram T'Kanchi <pedram_t'kanchi@intuit.com>
2020-01-01 12:41:10 -0800

    Updated react boilerplate

--- create-react-boilerplate.sh
+++ create-react-boilerplate.sh
@@ -3,50 +3,24 @@
 mkdir -p $1
 cd $1
 
-mkdir -p dist
-
 mkdir -p src
-mkdir -p src/app
-mkdir -p src/app/js
-mkdir -p src/app/js/components
-mkdir -p src/app/js/constants
-mkdir -p src/app/js/utils
-mkdir -p src/app/sass
+mkdir -p src/js
+mkdir -p src/js/components
 
-mkdir -p __tests__
-mkdir -p __tests__/component
-mkdir -p __tests__/unit
-
-touch src/app/js/index.js
-
-echo "
-/* eslint-disable import/no-extraneous-dependencies */
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
-
-" > src/setupTests.js
-
-echo "* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-" > src/app/sass/default.scss
+touch src/js/index.js
 
 echo "<!DOCTYPE html>
 <html>
   <head>
     <meta charset=\"UTF-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-    <title>Practice</title>
+    <title>$1</title>
   </head>
   <body>
     <div id=\"root\"></div>
   </body>
 </html>
-" > src/app/index.html
+" > src/index.html
 
 echo "const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -59,7 +33,7 @@ module.exports = {
     contentBase: './dist'
   },
   entry: {
-    app: './src/app/js/index.js',
+    app: './src/js/index.js',
   },
   output: {
     filename: '[name].bundle.js',
@@ -70,7 +44,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: '$1',
-      template: './src/app/index.html'
+      template: './src/index.html'
     }),
     new webpack.HotModuleReplacementPlugin()
   ],
@@ -104,87 +78,49 @@ module.exports = {
     ]
   },
   resolve: {
-    modules: [path.resolve(__dirname, './src'), path.resolve(__dirname, './node_modules')],
+    modules: [path.resolve(__dirname, './'), path.resolve(__dirname, './node_modules')],
     extensions: ['.js', '.jsx']
   }
 }; " > webpack.config.js
 
 echo "{
-  \"presets\": [\"@babel/preset-react\", \"@babel/env\"]
+  \"presets\": [\"@babel/preset-react\", \"@babel/env\"],
+  \"plugins\": [\"@babel/plugin-transform-runtime\", \"@babel/plugin-proposal-class-properties\", \"@babel/plugin-syntax-dynamic-import\"],
 }
 " > .babelrc
 
 echo "{
-  module.exports = {
-    env: {
-      browser: true,
-      es6: true
-    },
-    extends: ['airbnb'],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly'
-    },
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
-      },
-      ecmaVersion: 2018,
-      sourceType: 'module'
-    },
-    plugins: ['react'],
-    rules: {}
-  };
-}
-" > .eslintrc.js
-
-echo "{
-  \"name\": \"project-boilerplate\",
+  \"name\": \"react-boilerplate\",
   \"version\": \"1.0.0\",
   \"private\": true,
   \"license\": \"MIT\",
   \"scripts\": {
     \"build\": \"webpack\",
-    \"start\": \"webpack-dev-server --open\",
-    \"test\": \"jest\"
-  },
-  \"jest\": {
-    \"setupFilesAfterEnv\": [\"<rootDir>/src/setupTests\"],
-    \"modulePaths\": [
-      \"<rootDir>/src\"
-    ]
+    \"start\": \"webpack-dev-server --open\"
   },
   \"devDependencies\": {
+    \"@babel/plugin-proposal-class-properties\": \"*\",
+    \"@babel/plugin-transform-runtime\": \"*\",
+    \"@babel/plugin-syntax-dynamic-import\": \"*\",
     \"@babel/core\": \"^7.5.4\",
     \"@babel/preset-env\": \"^7.5.4\",
     \"@babel/preset-react\": \"^7.0.0\",
-    \"babel-jest\": \"^24.8.0\",
     \"babel-loader\": \"^8.0.6\",
     \"clean-webpack-plugin\": \"^3.0.0\",
-    \"css-loader\": \"^3.0.0\",
-    \"enzyme\": \"^3.10.0\",
-    \"enzyme-adapter-react-16\": \"^1.14.0\",
     \"file-loader\": \"^4.0.0\",
     \"html-webpack-plugin\": \"^3.2.0\",
-    \"jest\": \"^24.8.0\",
-    \"node-sass\": \"^4.12.0\",
-    \"react-test-renderer\": \"^16.8.6\",
-    \"sass-loader\": \"^7.1.0\",
-    \"style-loader\": \"^0.23.1\",
     \"webpack\": \"^4.35.0\",
     \"webpack-cli\": \"^3.3.4\",
     \"webpack-dev-server\": \"^3.7.2\",
-    \"eslint\": \"^5.16.0\",
-    \"eslint-config-airbnb\": \"^17.1.1\",
-    \"eslint-plugin-import\": \"^2.18.0\",
-    \"eslint-plugin-jsx-a11y\": \"^6.2.3\",
-    \"eslint-plugin-react\": \"^7.14.2\"
+    \"css-loader\": \"^3.4.0\",
+    \"node-sass\": \"^4.13.0\",
+    \"sass-loader\": \"^8.0.0\",
+    \"style-loader\": \"^1.1.2\"
   },
   \"dependencies\": {
-    \"react\": \"^16.8.6\",
-    \"react-dom\": \"^16.8.6\",
-    \"styled-components\": \"^4.3.2\",
-    \"react-router-dom\": \"^5.0.1\"
+    \"react\": \"*\",
+    \"react-dom\": \"*\",
+    \"styled-components\": \"*\"
   }
 }" > package.json
 
